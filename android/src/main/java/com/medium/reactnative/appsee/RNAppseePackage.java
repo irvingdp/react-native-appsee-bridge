a5d901f87c4a203e92314a64c2189e8c8e8d2f2c
irvingdp <irvingdp@bitbucket.org>
2017-02-08 14:53:46 +0800

    Ignore "Main" screen that auto detect by appsee sdk.

--- android/src/main/java/com/medium/reactnative/appsee/RNAppseePackage.java
+++ android/src/main/java/com/medium/reactnative/appsee/RNAppseePackage.java
@@ -17,6 +17,33 @@ public class RNAppseePackage implements ReactPackage {
 
     public RNAppseePackage(String apiKey) {
         mApiKey = apiKey;
+        Appsee.addAppseeListener(new AppseeListener() {
+            @Override
+            public void onAppseeScreenDetected(AppseeScreenDetectedInfo screenInfo)
+            {
+                //Ignore "Main" screen that auto detect by appsee sdk.
+                if(screenInfo.getScreenName().equals("MainActivity")){
+                    screenInfo.setScreenName(null);
+                }
+            }
+            @Override
+            public void onAppseeSessionStarting(AppseeSessionStartingInfo startingInfo)
+            {
+            }
+            @Override
+            public void onAppseeSessionStarted(AppseeSessionStartedInfo sessionInfo)
+            {
+            }
+
+            @Override
+            public void onAppseeSessionEnding(AppseeSessionEndingInfo sessionInfo)
+            {
+            }
+            @Override
+            public void onAppseeSessionEnded(AppseeSessionEndedInfo sessionInfo)
+            {
+            }
+        });
     }
 
     /**
