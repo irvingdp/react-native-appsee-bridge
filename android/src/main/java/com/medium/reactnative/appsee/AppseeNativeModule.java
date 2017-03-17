270a40f6c01f6f8154004d7279eacf296decd15f
Erwin <nkaloli@gmail.com>
2017-03-17 17:55:26 +0800

    (android) added argument:properties for addEvent

--- android/src/main/java/com/medium/reactnative/appsee/AppseeNativeModule.java
+++ android/src/main/java/com/medium/reactnative/appsee/AppseeNativeModule.java
@@ -4,10 +4,15 @@ import com.appsee.Appsee;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+
 import android.util.Log;
 
-class AppseeNativeModule extends ReactContextBaseJavaModule {
+import org.json.JSONException;
 
+import java.util.Map;
+
+class AppseeNativeModule extends ReactContextBaseJavaModule {
     public static final String NAME = "RNAppsee";
 
     public AppseeNativeModule(ReactApplicationContext reactContext) {
@@ -26,8 +31,13 @@ class AppseeNativeModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void addEvent(String eventName) {
+    public void addEvent(String eventName, ReadableMap properties) throws JSONException {
         Log.d("appsee", eventName);
-        Appsee.addEvent(eventName);
+        Map<String, Object> propertiesMap = ReactNativeJson.convertReadableMapToHashMap(properties);
+        if (propertiesMap.size() > 0) {
+            Appsee.addEvent(eventName, propertiesMap);
+        } else {
+            Appsee.addEvent(eventName);
+        }
     }
 }
